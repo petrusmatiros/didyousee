ce6fdcbc61d73eac170eafcde40944b8174077f8
Petrus Matiros <petrus.matiros@gmail.com>
2023-04-04 17:05:04 +0200

    Update main.ts style.css in src/ and add express code in server/

---
+++ server/server.ts
@@ -0,0 +1,9 @@
+// import express, { Application, Request, Response } from 'express';
+
+// const app: Application = express();
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello World!');
+// });
+
+// export default app;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import './index.css'
+
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
