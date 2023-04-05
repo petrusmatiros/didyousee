8373ae5af4fdc1eb55e7fbc5850813c90e062157
Petrus Matiros <petrus.matiros@gmail.com>
2023-04-05 16:45:07 +0200

    Remove postcss config and get project working

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-import './index.css'
+import './style.css'
 
 const app = createApp(App)
-app.use(router)
+app.use(router as any)
 app.use(createPinia())
 app.mount('#app')
