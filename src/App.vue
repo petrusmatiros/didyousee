3c33bde7b860788c314cb9339fd0459694db318c
Petrus Matiros <petrus.matiros@gmail.com>
2023-04-07 21:13:56 +0200

    refactor: make footer stay at bottom; adjust footer layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="flex-row flex-center" id="nav">
-    <router-link to="/" class="nav-link" id="logo"><img src="./assets/didyousee.svg" alt="didyousee logo. an owl holding a binocular, looking for the next movie/series" decoding="sync" fetchpriority="high"/></router-link>
+    <router-link to="/" class="nav-link flex-row" id="logo"><img src="./assets/didyousee.svg" alt="didyousee logo. an owl holding a binocular, looking for the next movie/series" decoding="sync" fetchpriority="high"/></router-link>
     <router-link to="/" class="nav-link">Watchlist</router-link>
     <router-link to="/" class="nav-link">Seen</router-link>
     <router-link to="/" class="nav-link">Liked</router-link>
@@ -8,9 +8,22 @@
     <router-link to="/profile" class="nav-link" id="profile">Profile</router-link>
   </nav>
   <router-view/>
-  <footer class="flex-row flex-seperate" id="footer">
-  <p>Copyright © 2023 didyousee. All rights reserved</p>
-  <div class="flex-row">
+  <footer class="flex-col flex-seperate" id="footer">
+    <div class="footer-top flex-row flex-center">
+      <router-link to="/" class="nav-link flex-row" id="logo"><img src="./assets/didyousee.svg" alt="didyousee logo. an owl holding a binocular, looking for the next movie/series" decoding="sync" fetchpriority="high"/></router-link>
+      <router-link to="/" class="nav-link">Watchlist</router-link>
+      <router-link to="/" class="nav-link">Seen</router-link>
+      <router-link to="/" class="nav-link">Liked</router-link>
+      <router-link to="/" class="nav-link">Disliked</router-link>
+      <router-link to="/profile" class="nav-link"> Profile</router-link>
+    </div>
+    <div class="footer-bottom flex-row flex-center">
+
+    </div>
+  <div class="flex-row flex-center">
+    <p>Copyright © 2023 didyousee. All rights reserved</p>
+  </div> 
+  <div class="flex-row flex-center">
     <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
     <img src="./assets/tmdb_alt_short_blue.svg" alt="TMDB alt short logo" loading="lazy" decoding="async"/>
   </div>
