b07996692653a26886db065aada86ed028fc4bd3
maxisr01 <maxism29.mi@gmail.com>
2023-04-12 20:37:16 +0200

    Continued with Filter

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="search-filter">
-    <h2>Search filter</h2>
+    <h2>Search Filter</h2>
+    <h3>Language</h3>
+    <h3>Region</h3>
+    <h3>Year</h3>
+    <h3>Adult</h3>
+    <button>Clear</button>
+    <button>Search</button>
   </div>
 </template>
 
@@ -20,5 +26,34 @@ export default defineComponent({
   width: 33%; 
   height: 100%; 
   background-color: red;
+}
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 16px;
+}
+
+h3 {
+  font-size: 18px;
+  margin-bottom: 8px;
+}
+
+button {
+  background-color: #007bff;
+  color: #fff;
+  padding: 8px 16px;
+  font-size: 16px;
+  border: none;
+  margin-top: 16px;
+  margin-right: 8px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+button:active {
+  background-color: #004380;
 }
 </style>
