8f9573eae8849e56adfb6d98b67ce69e4260f60e
maxisr01 <maxism29.mi@gmail.com>
2023-04-20 23:15:45 +0200

    Update SearchResultsView.vue

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -14,13 +14,13 @@ function searchClickACB() { emit("searchClick") }
   <!-- <button @click="searchClickACB()">X</button> -->
   <div class="search-results flex-col flex-center">
     <div class="trending-list flex-row flex-center">
-      <div>
+      <div v-if="$props.model.movies.length > 0">
         <h1>Movies</h1>
       </div>
       <div class="trending-list flex-row flex-center">
         <MediaCard v-for="(movie, index) in $props.model.movies" :key="index" :media="movie" :media-type="'movie'" />
       </div>
-      <div>
+      <div v-if="$props.model.series.length > 0">
         <h1>Series</h1>
       </div>
       <div class="trending-list flex-row flex-center">
