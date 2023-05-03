4884cb2f95e97079f4993d6540ac47469ad11740
Petrus Matiros <petrus.matiros@gmail.com>
2023-05-03 23:27:30 +0200

    refactor: attempt to add Suspense

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -1,39 +1,46 @@
 <script setup lang="ts">
 import './../style.css';
 import MediaCard from '../components/MediaCard.vue';
+import { Suspense } from 'vue';
 
 </script>
 
 <template>
   <Suspense>
-  <!-- <button @click="searchClickACB()">X</button> -->
-  <div  class="search-results flex-col flex-center gap-half">
-
-    <!-- Result -->
-    <div v-if="result_status === 'fulfilled'" class="trending-list gap-full flex-row flex-center">
-      <MediaCard v-for="(media, index) in $props.model.searchContent" :key="index" :media="media" :media-type="media.mediaType" />
-    </div> 
-
-     <!-- Invalid search -->
-    <div v-else-if="result_status === 'rejected'">
-      <div class="flex-col flex-center gap-half">
-        <p>Searching for "{{ $props.model.searchString }}" returned no matches.</p> 
-        <p>Was it a typo? :p</p> 
-      </div>
-    </div>
-
-    <!-- TODO: Dummy content -->
-    <div v-else class="trending-list gap-full flex-row flex-center">
-      <div class="loading-skeleton content-card-dummy gap-full flex-col" v-for="(media, index) in $props.model.dummyContent" :key="index" :media="media" :media-type="media.mediaType">
-        <div class="content-poster nothing"></div>
-        <div class="flex-col flex-center gap-half">
-          <h2 class="flex-row"></h2>
-          <div class="flex-row flex-center gap-half"></div>
+    <template #default>
+      <!-- <button @click="searchClickACB()">X</button> -->
+      <div  class="search-results flex-col flex-center gap-half">
+    
+        <!-- Result -->
+        <div v-if="result_status === 'fulfilled'" class="trending-list gap-full flex-row flex-center">
+          <MediaCard v-for="(media, index) in $props.model.searchContent" :key="index" :media="media" :media-type="media.mediaType" />
+        </div> 
+    
+         <!-- Invalid search -->
+        <div v-else-if="result_status === 'rejected'">
+          <div class="flex-col flex-center gap-half">
+            <p>Searching for "{{ $props.model.searchString }}" returned no matches.</p> 
+            <p>Was it a typo? :p</p> 
+          </div>
         </div>
+    
+        <!-- TODO: Dummy content -->
+        <!-- <div v-else class="trending-list gap-full flex-row flex-center">
+          <div class="loading-skeleton content-card-dummy gap-full flex-col" v-for="(media, index) in $props.model.dummyContent" :key="index" :media="media" :media-type="media.mediaType">
+            <div class="content-poster nothing"></div>
+            <div class="flex-col flex-center gap-half">
+              <h2 class="flex-row"></h2>
+              <div class="flex-row flex-center gap-half"></div>
+            </div>
+          </div>
+        </div> -->
       </div>
-    </div>
-
-  </div>
+    </template>
+    <template #fallback>
+      <div class="loading-skeleton">
+        LOADING THIS IS LOADING
+      </div>
+    </template>
 </Suspense>
 </template>
 
