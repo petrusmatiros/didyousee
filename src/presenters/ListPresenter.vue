d2d9ee2cdae15415f48222d3dc74990a89bfc447
Petrus Matiros <petrus.matiros@gmail.com>
2023-05-05 18:43:16 +0200

    feat: add list view and presenter

---
+++ src/presenters/ListPresenter.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ListView from "../views/ListView.vue";
+
+export default defineComponent({
+    name: "ListPresenter",
+    components: {
+        ListView,
+    },
+    setup(props: any) {
+
+        return {
+        };
+    },
+});
+</script>
+<template>
+    <ListView/>
+</template>
